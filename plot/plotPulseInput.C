c30f0fb099e979190bb051e6b93fe06835383673
amassiro <massironi.andrea@gmail.com>
2015-10-07 02:58:11 +0200

    now it works

---
+++ plot/plotPulseInput.C
@@ -0,0 +1,46 @@
+//---- plot output of multifit
+
+void plotPulseInput (std::string nameInputFile = "output.root"){
+ 
+ TFile *file = new TFile(nameInputFile.c_str());
+ 
+ TTree* tree = (TTree*) file->Get("Samples");
+  
+ int    nWF;
+ std::vector<double>* waveform = new std::vector<double>;
+ std::vector<double>* samples = new std::vector<double>;
+ 
+ tree->SetBranchAddress("nWF",      &nWF);
+ tree->SetBranchAddress("waveform", &waveform);
+ tree->SetBranchAddress("samples", &samples);
+ 
+ tree->GetEntry(10);
+ 
+ TCanvas* ccwaveform = new TCanvas ("ccwaveform","",800,600);
+ TGraph *gr = new TGraph();
+ for(int i=0; i<nWF; i++){
+  gr->SetPoint(i, i, waveform->at(i));
+ }
+ gr->Draw("AL");
+ gr->SetLineColor(kMagenta);
+ gr->SetLineWidth(2);
+ gr->GetXaxis()->SetTitle("time [ns]");
+ 
+ 
+ TCanvas* ccPulse = new TCanvas ("ccPulse","",800,600);
+ TGraph *grPulse = new TGraph();
+ for(int i=0; i<samples->size(); i++){
+  grPulse->SetPoint(i, i, samples->at(i));
+ }
+ grPulse->SetMarkerSize(2);
+ grPulse->SetMarkerStyle(21);
+ grPulse->SetMarkerColor(kRed);
+ grPulse->Draw("ALP");
+ grPulse->GetXaxis()->SetTitle("BX");
+ 
+ 
+ 
+ 
+}
+
+

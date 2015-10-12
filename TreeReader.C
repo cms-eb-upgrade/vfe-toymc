6b76e21cc72ff439f63b715db61480260162b435
Alex Coda <coda.a@husky.neu.edu>
2015-10-12 14:01:29 +0200

    Made some changes

--- TreeReader.C
+++ TreeReader.C
@@ -4,7 +4,7 @@
 
   Source:
   https://github.com/cms-eb-upgrade/vfe-toymc
-
+mysample_100_0_10_25.00.root
   To run:
   root -l -q TreeReader.C
 */
@@ -20,42 +20,39 @@ void TreeReader(const char* file = "output.root") {
   TTree* tree = (TTree*) fin->Get(fin->GetListOfKeys()->At(0)->GetName());
 
   // For actually filling the waveform
-  std::vector<float>* waveformF;   // Used if the source data type is float vector
-  double waveformD[WFLENGTH];      // Used if the source data type is double array
-  double entry;                    // Helper for the float vector case
+  std::vector<double>* waveformV;  // Used if the source data type is double vector
+  double waveformA[WFLENGTH];      // Used if the source data type is double array
   int events = tree->GetEntries(); // The total events stored in the tree to be iterated over
-  bool isFloatVector = (strcmp(tree->GetBranch("waveform")->GetClassName(), "vector<float>") == 0);
+  bool isVector = (strcmp(tree->GetBranch("waveform")->GetClassName(), "vector<double>") == 0);
 
   // Determine the correct data type from the input file
-  if (isFloatVector) {
-    tree->SetBranchAddress("waveform", &waveformF);
+  if (isVector) {
+    tree->SetBranchAddress("waveform", &waveformV);
   } 
   else {
-    tree->SetBranchAddress("waveform", &waveformD);
+    tree->SetBranchAddress("waveform", &waveformA);
   }
 
   for (int e = 0; e < events; e++) {
     if ((e + 1) % 10 == 0) cout << "Event: " << e + 1 << " / " << events << endl;
     // Create a new hist for the current event
-    TH1D* wavehist = new TH1D("Event_" + TString::Itoa(e, 10), 
-                              "Waveform with pileup; Time (ns); Amplitude (GeV)",
-                              WFLENGTH, 0, WFLENGTH);
+    TH1D wavehist("Event_" + TString::Itoa(e, 10), 
+                  "Waveform with pileup; Time (ns); Amplitude (GeV)",
+                  WFLENGTH, 0, WFLENGTH);
+
     tree->GetEntry(e);
     // Fill the histogram across the whole time range
     for (int t = 0; t < WFLENGTH; t++) {
-      if (isFloatVector) {
-        entry = static_cast<double>(waveformF->at(t));
-        wavehist->Fill(t, entry);
-        cout << "Un-cast: " << waveformF->at(t) << endl;
-        cout << "Entry: " << entry << endl;
+      if (isVector) {
+        wavehist.Fill(t, waveformV->at(t));
       }
       else {
-        wavehist->Fill(t, waveformD[t]);
+        wavehist.Fill(t, waveformA[t]);
       }
-      wavehist->SetBinError(t, 0.0);
+      wavehist.SetBinError(t, 0.0);
     }
     fout->cd();
-    wavehist->Write();
+    wavehist.Write();
   }
 
   fout->Close();
